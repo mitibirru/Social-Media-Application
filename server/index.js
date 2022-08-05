9e8ef5c0d697ba840ef84c61b744a65296be7c63
mitibirru <357miteshbirru@gmail.com>
2022-08-05 18:34:58 +0530

    added like and delete functionality, updated constants for irregular security concerns

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -12,7 +14,8 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb://localhost:27017/socialmedia";
+const CONNECTION_URL =
+  "mongodb://localhost:27017/socialmedia" || process.env.CONNECTION_URL;
 
 const PORT = process.env.PORT || 5000;
 
